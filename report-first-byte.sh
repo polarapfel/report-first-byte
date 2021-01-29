c643ab710b1401c9b34f98bc501155782a1725c8
Tobias <tobias@weisserth.net>
2021-01-28 17:10:36 -0800

    Add files via upload

---
+++ report-first-byte.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while getopts u:n:l:s: flag
+do
+    case "${flag}" in
+        u) uri=${OPTARG};;
+        n) requests=${OPTARG};;
+        l) label=${OPTARG};;
+        s) wsleep=${OPTARG};;
+    esac
+done
+
+touch "${label}-report.csv"
+echo -e "${label}\n" >> ${label}-report.csv
+echo -e "Uri: $uri\n" >> ${label}-report.csv
+cat report-header-csv.txt >> ${label}-report.csv
+
+x=$requests
+while [ $x -gt 0 ];
+do
+    curl -w "@curl-format-csv.txt" -r 0-1 -o /dev/null -s $uri >> ${label}-report.csv
+    sleep $wsleep
+    x=$(($x-1))
+done
